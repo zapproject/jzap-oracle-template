32f992a2faa02114c81d0ca2bd15bebcfe6006cb
AlexD <taejoonn@yahoo.com>
2021-04-06 04:46:58 -0400

    Removed status

--- src/test/java/io/github/oracle/template/jzap/StatusTest.java
+++
@@ -1,9 +0,0 @@
-package io.github.oracle.template.jzap;
-
-
-public class StatusTest {
-    // @Test
-    // void updateStatusTest() {
-    //     Status.updateStatus(Web3j.build(new HttpService()), );
-    // }
-}
